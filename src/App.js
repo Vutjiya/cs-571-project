9bddb89a974a52c87faa9be93ceed46570b09f65
dannycedrone <99098617+dannycedrone@users.noreply.github.com>
2025-03-30 11:40:37 -0400

    basic app with placeholder routes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Routes, Route } from 'react-router';
+
+function App() {
+  return (
+    <>
+    <Sidebar className='sidebar'/>
+    <Routes>
+      <Route path="/q1" element={<Question1 />} />
+      <Route path="/q2" element={<Question2 />} />
+      <Route path="/q3" element={<Question3 />} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <p>Intro</p>
+            <p>Q1</p>
+            <p>Q2</p>
+            <p>Q3</p>
+        </div>
+    )
+}
+
+export default Sidebar
