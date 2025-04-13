427d26de1c043f766f6637eb8207184f005d390f
Vutjiya <vsenabunyari@umass.edu>
2025-04-13 19:45:29 -0400

    added data for 2nd topic

--- data/cleaning_script.R
+++ data/cleaning_script.R
@@ -99,6 +99,117 @@ crimes_data <- national_crimes %>%
   group_by(year, crime) %>%
   summarize(incidents = sum(incidents))
 
-bind_rows(list(migrants = migrant_data, national = crimes_data), .id = "id") %>%
+crime_comparison_data <- bind_rows(
+  list(migrants = migrant_data, national = crimes_data),
+  .id = "source"
+)
+
+crime_comparison_data %>%
   toJSON(pretty = TRUE) %>%
-  write_json(path = "crime_incidents.json")
+  write_json(path = "crime_comparison.json")
+
+
+total_crime_1 <- national_crimes %>%
+  filter(is.na(state_name) & is.na(state_abbr))
+
+total_crime_2 <- national_crimes %>%
+  filter(between(year, 2017, 2020)) %>%
+  group_by(year) %>%
+  summarize(across(where(is.numeric), sum))
+
+total_crime_3 <- national_crimes %>%
+  filter(state_name == "United States Total")
+
+total_crime_data <- list(total_crime_1, total_crime_2, total_crime_3) %>%
+  bind_rows() %>%
+  select(!c(starts_with("state"), caveats, rape_legacy)) %>%
+  mutate(across(everything(), \(col) replace_na(col, replace = 0))) %>%
+  pivot_longer(
+    cols = !c(year, population),
+    names_to = "crime",
+    values_to = "incidents"
+  ) %>%
+  group_by(year) %>%
+  summarize(incidents = sum(incidents))
+
+total_crime_data %>%
+  toJSON(pretty = TRUE) %>%
+  write_json(path = "total_crime.json")
+
+
+# crime_comparison_data %>%
+#   ggplot(aes(x = year, y = incidents)) +
+#   geom_col(aes(fill = crime), position = "dodge") +
+#   scale_y_log10() +
+#   facet_wrap(~id)
+
+# crime_comparison_data %>%
+#   filter(source == "migrants") %>%
+#   ggplot(aes(x = year, y = incidents, color = crime)) +
+#   geom_line(aes(group = crime))
+
+# crime_comparison_data %>%
+#   filter(source == "migrants" & between(year, 2019, 2021))
+
+# crime_comparison_data %>%
+#   filter(source == "migrants") %>%
+#   tail()
+
+gdp <- read_csv("gdp_data.csv")
+dpi <- read_csv("dpi_data.csv")
+cpi <- read_csv("cpi_data.csv")
+
+gdp_data <- gdp %>%
+  select(-c(1:3)) %>%
+  rename(
+    any_of(set_names(colnames(.), gdp %>% select(-c(1:3)) %>% slice(7)))
+  ) %>%
+  slice(8) %>%
+  select(contains("Q2")) %>%
+  pivot_longer(everything(), names_to = "year", values_to = "gdp") %>%
+  mutate(
+    year = as.numeric(str_replace(year, "Q2$", "")),
+    gdp_change = as.numeric(gdp)
+  )
+
+dpi_data <- dpi %>%
+  slice(c(7, 54)) %>%
+  select(-c(1:3)) %>%
+  mutate(
+    across(.cols = where(\(x) !is.numeric(x)), .fns = \(x) as.numeric(x))
+  ) %>%
+  rename(any_of(set_names(colnames(.), 1929:2024))) %>%
+  slice(2) %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = "year",
+    values_to = "dpi_change",
+    names_transform = \(x) as.numeric(x)
+  )
+
+cpi_data <- cpi %>%
+  rename(any_of(set_names(colnames(.), cpi %>% slice(11)))) %>%
+  select(-c(HALF1, HALF2)) %>%
+  slice(-c(1:11)) %>%
+  pivot_longer(cols = !Year, names_to = "month", values_to = "cpi") %>%
+  group_by(Year) %>%
+  summarise(cpi = mean(as.numeric(cpi))) %>%
+  mutate(cpi_change = (cpi / lag(cpi) - 1) * 100, Year = as.numeric(Year))
+
+# gdp_data %>%
+#   tail(10)
+
+# cpi_data %>%
+#   tail(10)
+
+# dpi_data %>%
+#   tail(10)
+
+gdp_data %>%
+  write_csv("cleaned_gdp.csv")
+
+dpi_data %>%
+  write_csv("cleaned_dpi.csv")
+
+cpi_data %>%
+  write_csv("cleaned_cpi.csv")
